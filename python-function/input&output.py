1892f7a68fe0b4817861b00751ddb0d4489966a1
Gyuhyeok Lee <daniel208847@gmail.com>
2025-05-29 14:12:53 +0100

    input and ouput understanding

---
+++ python-function/input&output.py
@@ -0,0 +1,17 @@
+# user input example
+a = input()
+print("user input:", a)
+
+# user input example with prompt
+b = input("Please enter something: ")
+print("user input with prompt:", b)
+
+# user input example with number
+c = input("Please enter a number: ")
+print("user input as string:", c)
+print(type((c)))
+
+# user input example
+for i in range(10):
+    print(i, end=' ')
+ 
