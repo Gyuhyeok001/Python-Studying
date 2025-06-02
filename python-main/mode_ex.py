1fc64a18e39b701a16bfcafd301712e019e9180c
Gyuhyeok Lee <daniel208847@gmail.com>
2025-06-02 06:56:01 +0100

    module import practice

---
+++ python-main/mode_ex.py
@@ -0,0 +1,20 @@
+## This file is combined with module.py to demonstrate module usage.##
+
+# 1. EXAMPLE: mode_ex.py
+#def add(a,b):
+#    return a + b
+
+# 2. EXAMPLE: mode_ex.py
+PI = 3.141592
+
+class math:
+    def solve(self , r):
+        return PI * (r ** 2)
+
+def add(self, a, b):
+        return a + b
+
+# 3. EXAMPLE: mode_ex.py directing outside the module
+# import sys
+# sys.path.append('././')  # Add the parent directory to the path
+ 
---
+++ python-main/module.py
@@ -0,0 +1,11 @@
+## This file is combined with mode_ex.py to demonstrate module usage.##
+
+# 1. module.py
+#import mode_ex
+#print(mode_ex.add(2, 3))
+
+import mode_ex
+print(mode_ex.PI)
+a = mode_ex.math()
+print(a.solve(5))
+
